73dbf83ccceee1f3466c88e1bd52c9decadd497a
shanlihang <67490009@qq.com>
2025-05-29 19:27:26 +0800

    feat：搭建智慧鱼缸系统基础工程

---
+++ smart-aquarium/src/pages/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <view class="index">
+    <nut-navbar title="智能鱼缸系统"></nut-navbar>
+    <nut-button type="primary" @click="onClick">按钮</nut-button>
+    <nut-toast v-model:visible="show" msg="你成功了" />
+  </view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const show = ref(false);
+const onClick = () => {
+  show.value = true;
+};
+</script>
