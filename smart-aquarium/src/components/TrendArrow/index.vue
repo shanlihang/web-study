38a71dba30777039914957600d9a4be4022839d3
shanlihang <67490009@qq.com>
2025-05-29 22:00:41 +0800

    feat：封装趋势箭头组件

---
+++ smart-aquarium/src/components/TrendArrow/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <view class="trend">
+    {{ props.number }}
+    <IconFont
+      v-if="props.status === 'high'"
+      name="triangle-up"
+      color="#fa2c19"
+    />
+    <IconFont
+      v-if="props.status === 'low'"
+      name="triangle-down"
+      color="#fa2c19"
+    />
+  </view>
+</template>
+
+<script setup>
+import { IconFont } from "@nutui/icons-vue-taro";
+const props = defineProps({
+  number: {
+    type: Number,
+    default: 0,
+  },
+  status: {
+    type: String,
+    default: "normal",
+  },
+});
+</script>
+
+<style scoped>
+.trend {
+  display: flex;
+  align-items: end;
+  justify-content: center;
+  gap: 12px;
+}
+</style>
