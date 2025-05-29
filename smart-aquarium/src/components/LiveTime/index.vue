dc13b167f0c5989f492606f7fbbe8b3689dc39a2
shanlihang <67490009@qq.com>
2025-05-29 22:00:55 +0800

    feat：封装实时数据组件

---
+++ smart-aquarium/src/components/LiveTime/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <view class="container">
+    <view>实时监控</view>
+    <image src="../../assets/fish.gif" mode="widthFix" class="gif-image" />
+
+    <nut-grid :column-num="2" reverse>
+      <nut-grid-item :border="false" text="水温(°C)">
+        <TrendArrow :number="10" status="high" />
+      </nut-grid-item>
+      <nut-grid-item :border="false" text="水质(ppm)">
+        <TrendArrow :number="10" status="low" />
+      </nut-grid-item>
+    </nut-grid>
+  </view>
+</template>
+
+<script setup>
+import TrendArrow from "../TrendArrow";
+</script>
+
+<style scoped>
+.container {
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+}
+.gif-image {
+  width: 100%;
+  height: 350px;
+  margin: 12px 0 24px;
+  border-radius: 12px;
+  overflow: hidden;
+}
+</style>
--- smart-aquarium/src/pages/index/index.vue
+++ smart-aquarium/src/pages/index/index.vue
@@ -2,12 +2,14 @@
   <view class="index">
     <nut-navbar title="智能鱼缸系统"></nut-navbar>
     <StatusCard :status="data.status" @reconnect="reconnect" />
+    <LiveTime />
   </view>
 </template>
 
 <script setup>
 import { reactive, ref } from "vue";
 import StatusCard from "../../components/StatusCard";
+import LiveTime from "../../components/LiveTime";
 
 const data = reactive({
   status: "error",
